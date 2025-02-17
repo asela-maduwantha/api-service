acb395fe16570313daf9e1eb66f3de008973165c
asela-maduwantha <maduwanthaaselagra@gmail.com>
2025-02-17 10:21:47 +0530

    update dto

--- src/main/scala/dto/Employee.scala
+++ src/main/scala/dto/Employee.scala
@@ -5,7 +5,7 @@ import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.write
 import org.json4s.{Formats, NoTypeHints}
 
-case class Employee(id: Option[Int], name: String, email: String)
+case class Employee(id: Option[Int], name: String, email: String, address: String, salary: Double)
 
 object Employee {
   implicit val formats: Formats = Serialization.formats(NoTypeHints)
